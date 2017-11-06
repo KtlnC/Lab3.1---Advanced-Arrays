6535c4fa50d05d38e27adbc49edcb051f33df6d9
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-11-06 13:16:50 -0500

    first commit

---
+++ ArrayMethods/src/ArrayMethods.java
@@ -0,0 +1,28 @@
+/**
+ * Author: Kaitlyn Cao
+ * Created 11/6/17
+ * Lab 3.1 - Advanced Arrays
+ * */
+public class ArrayMethods {
+
+	public static void main (String[] args)
+	{
+		
+	}
+	
+	public static int[] removeDuplicates(int[] list)
+	{
+		int[] arr = list;
+		for(int i = 0; i < list.length; i++)
+		{
+			for(int j = i + 1; j < list.length; j++)
+			{
+				if(list[i] == list[j])
+				{
+					
+				}
+			}
+		}
+		return arr;
+	}
+}
